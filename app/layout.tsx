d52872d9ffbbc746e2cbc94c02deed28b1f6b555
viktorexe <developer.viktorexe@gmail.com>
2025-11-02 21:39:23 +0530

    debug

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,9 +41,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="manifest" href="/manifest.json" />
-      </head>
       <body>{children}</body>
     </html>
   )
