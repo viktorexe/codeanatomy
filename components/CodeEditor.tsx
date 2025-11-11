fcbc0107cdf8e51f9a47463455e670de9f81bb54
viktorexe <developer.viktorexe@gmail.com>
2025-11-11 23:56:18 +0530

    Project directory remade
    Newly made directory

--- components/CodeEditor.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-export function CodeEditor() {
-  // This component is handled by the main interface HTML template
-  // All functionality is managed through the MainInterface component
-  return null
-}
--- components/ConsoleOutput.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-export function ConsoleOutput() {
-  // This component is handled by the main interface HTML template
-  // All functionality is managed through the MainInterface component
-  return null
-}
